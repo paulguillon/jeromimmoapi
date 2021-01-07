3988d4276213e87290defc4afa7c32e5d039076d
Breesho <felix.noel3@orange.fr>
2021-01-07 15:00:48 +0100

    modif contrainte table roles

--- database/migrations/2021_01_06_132420_create_roles_table.php
+++ database/migrations/2021_01_06_132420_create_roles_table.php
@@ -20,9 +20,9 @@ class CreateRolesTable extends Migration
             $table->bigIncrements('idRole');
             $table->string('roleName');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->foreignId('created_by')->constrained('users', 'idUser');
+            $table->foreignId('created_by');
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
-            $table->foreignId('updated_by')->constrained('users', 'idUser');
+            $table->foreignId('updated_by');
         });
     }
 
