3c368a9a477dda3f9f4e776de2559c8db5569473
Paul Guillon <alentak27@gmail.com>
2021-02-05 12:02:21 +0100

    swagger plus autres choses j'imagine

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,12 @@ $app->withFacades();
 
 $app->withEloquent();
 
+
+// load the swagger-lume configuration
+$app->configure('swagger-lume');
+// register the lumen swagger service provider
+$app->register(\SwaggerLume\ServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
